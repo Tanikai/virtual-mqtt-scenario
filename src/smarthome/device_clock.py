82c2cf28ad9b05f370b898bf457e19a191e82f94
Kai Anter <kai.anter@web.de>
2022-07-15 12:26:50 +0200

    Added: clock loop

--- src/smarthome/device_clock.py
+++ src/smarthome/device_clock.py
@@ -1,18 +1,24 @@
 import tkinter as tk
 from .device_base import DeviceBase, DeviceBaseView
-from threading import Thread
+from threading import Thread, Event
+from datetime import time, timedelta
 
 
 class ClockGenerator(Thread):
+    callback = None
+    current_time = time(12, 0)
 
     def __init__(self, callback):
         super().__init__()
+        self.callback = callback
+        self.event = Event()  # for stopping
 
     def run(self):
-        while not self.event_is_set():
-            
-
-
+        while not self.event.is_set():
+            self.callback({"time": self.current_time.strftime("%H:%M")})
+            # increase 15 minutes every 1 second
+            self.current_time = self.current_time + timedelta(minutes=15)
+            self.event.wait(1)  # wait 1 second
 
 
 class DeviceClockView(DeviceBaseView):
@@ -26,5 +32,3 @@ class DeviceClock(DeviceBase):
 
     def __init__(self):
         super().__init__()
-
-    d
