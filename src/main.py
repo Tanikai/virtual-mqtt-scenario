57a8fcbbb648a66399ea1380d27ab0cd8e61d2c9
Kai Anter <kai@anter.dev>
2022-07-21 18:43:24 +0200

    Edit: moved sample scenario to own python file

--- src/main.py
+++ src/main.py
@@ -1,27 +1,9 @@
-import json
 from application import App
 import configparser
 import sys
 from os.path import exists
 from os import getcwd
-from smarthome.device_thermometer import DeviceThermometer, \
-    DeviceThermometerView
-from smarthome.device_clock import DeviceClock, DeviceClockView
-from smarthome.device_lamp import DeviceLamp, DeviceLampView
-from smarthome.device_remote import DeviceRemote, DeviceRemoteView
-from smarthome.device_window_blind import DeviceWindowBlind, \
-    DeviceWindowBlindView
-from smarthome.device_window import DeviceWindow, DeviceWindowView
-from smarthome.device_weather import DeviceWeather, DeviceWeatherView
-
-
-def remote_lights(self):
-    self.mqtt_client.publish("home0/living_room/ceiling_lamp/toggle_power",
-                             json.dumps({"sender": "remote"}))
-
-
-def init_clock(self):
-    self.generator.set_time(15, 0)
+from sample_scenario import init_sample_scenario
 
 
 def read_config_file(path: str) -> dict:
@@ -52,26 +34,8 @@ if __name__ == '__main__':
     app = App()
 
     c = get_config_dict()
-
-    h0 = "home0"
-    l = "living_room"
-    act_col = 1  # actuator column
-
-    # create new instance of DeviceThermometer, but only pass constructor
-    clock = DeviceClock(c, h0, "clock0")
-    clock.on_run = init_clock
-    app.add_device(clock, DeviceClockView)
-    app.add_device(DeviceThermometer(c, h0, l, "thermometer0"),
-                   DeviceThermometerView)
-    app.add_device(DeviceLamp(c, h0, l, "ceiling_lamp"), DeviceLampView, act_col)
-    app.add_device(DeviceLamp(c, h0, l, "standing_lamp"), DeviceLampView, act_col)
-    d = app.add_device(DeviceRemote(c, h0, l, "light_switch"),
-                       DeviceRemoteView)
-    d.set_text("Toggle Lights")
-    d.on_click = remote_lights
-
-    app.add_device(DeviceWindow(c, h0, l, "window0"), DeviceWindowView, act_col)
-    app.add_device(DeviceWindowBlind(c, h0, l, "window_blind"),
-                   DeviceWindowBlindView, act_col)
-    app.add_device(DeviceWeather(c, h0, "weather_sensor0"), DeviceWeatherView)
+    # init_sample_scenario creates a sample scenario with some devices. If you
+    # want to create your own virtual smart home scenario, remove/comment out
+    # the function and use app.add_device(...).
+    init_sample_scenario(app, c)
     app.run()
---
+++ src/sample_scenario.py
@@ -0,0 +1,52 @@
+from application import App
+from smarthome.device_thermometer import DeviceThermometer, \
+    DeviceThermometerView
+from smarthome.device_clock import DeviceClock, DeviceClockView
+from smarthome.device_lamp import DeviceLamp, DeviceLampView
+from smarthome.device_remote import DeviceRemote, DeviceRemoteView
+from smarthome.device_window_blind import DeviceWindowBlind, \
+    DeviceWindowBlindView
+from smarthome.device_window import DeviceWindow, DeviceWindowView
+from smarthome.device_weather import DeviceWeather, DeviceWeatherView
+import json
+
+
+def _remote_lights(self):
+    self.mqtt_client.publish("home0/living_room/ceiling_lamp/toggle_power",
+                             json.dumps({"sender": "remote"}))
+
+
+def _init_clock(self):
+    self.generator.set_time(15, 0)
+
+
+def init_sample_scenario(app: App, c: dict):
+    """
+    Creates a sample smart home scenario.
+    :param app: Application object
+    :param c: MQTT Broker configuration
+    :return:
+    """
+    h0 = "home0"
+    l = "living_room"
+    act_col = 1  # actuator column
+    clock = DeviceClock(c, h0, "clock0")
+    clock.on_run = _init_clock
+    # create new instance of DeviceThermometer, but only pass constructor
+    app.add_device(clock, DeviceClockView)
+    app.add_device(DeviceThermometer(c, h0, l, "thermometer0"),
+                   DeviceThermometerView)
+    app.add_device(DeviceLamp(c, h0, l, "ceiling_lamp"), DeviceLampView,
+                   act_col)
+    app.add_device(DeviceLamp(c, h0, l, "standing_lamp"), DeviceLampView,
+                   act_col)
+    d = app.add_device(DeviceRemote(c, h0, l, "light_switch"),
+                       DeviceRemoteView)
+    d.set_text("Toggle Lights")
+    d.on_click = _remote_lights
+
+    app.add_device(DeviceWindow(c, h0, l, "window0"), DeviceWindowView,
+                   act_col)
+    app.add_device(DeviceWindowBlind(c, h0, l, "window_blind"),
+                   DeviceWindowBlindView, act_col)
+    app.add_device(DeviceWeather(c, h0, "weather_sensor0"), DeviceWeatherView)
